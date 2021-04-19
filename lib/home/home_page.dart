beaf3480dd77b7ddda96916c9e4156ee574cc058
Felipe Sales <[soufeliposales@gmail.com]>
2021-04-19 12:44:18 -0300

    Liftoff

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,10 +1,18 @@
+import 'package:devquiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBarWidget(),
     );
   }
 }
---
+++ lib/home/widgets/chart/chart_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:devquiz/core/app_colors.dart';
+import 'package:devquiz/core/app_text_styles.dart';
+
+class ChartWidget extends StatelessWidget {
+  const ChartWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      width: 80,
+      child: Stack(
+        children: [
+          Center(
+            child: Container(
+              height: 80,
+              width: 80,
+              child: CircularProgressIndicator(
+                strokeWidth: 10,
+                value: .75,
+                backgroundColor: AppColors.chartSecondary,
+                valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+              ),
+            ),
+          ),
+          Center(child: Text('75%', style: AppTextStyles.heading)),
+        ],
+      ),
+    );
+  }
+}
