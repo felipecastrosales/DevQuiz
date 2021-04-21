ef7ab35390cfc25615b54fbafe165a5e9b4db4c0
Felipe Sales <[soufeliposales@gmail.com]>
2021-04-21 12:30:21 -0300

    Updates with Models in Application for Answers, Questions, Quizzes and Users

---
+++ lib/shared/models/answer_model.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+class AnswerModel {
+  final String title;
+  final bool isRight;
+
+  AnswerModel({required this.title, this.isRight = false});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'isRight': isRight,
+    };
+  }
+
+  factory AnswerModel.fromMap(Map<String, dynamic> map) {
+    return AnswerModel(
+      title: map['title'],
+      isRight: map['isRight'] ?? false,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AnswerModel.fromJson(String source) => 
+      AnswerModel.fromMap(json.decode(source));
+}
+
---
+++ lib/shared/models/question_model.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'answer_model.dart';
+
+class QuestionModel {
+  final String title;
+  final List<AnswerModel> answers;
+
+  QuestionModel({required this.title, required this.answers})
+      : assert(answers.length == 4);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'answers': answers.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory QuestionModel.fromMap(Map<String, dynamic> map) {
+    return QuestionModel(
+      title: map['title'],
+      answers: List<AnswerModel>.from(map['answers']?.map(
+        (x) => AnswerModel.fromMap(x)),
+      ),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuestionModel.fromJson(String source) => 
+      QuestionModel.fromMap(json.decode(source));
+}
