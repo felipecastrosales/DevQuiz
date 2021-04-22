849dafd9dac2068b81326f90a69b72cf2ea19030
Felipe Sales <[soufeliposales@gmail.com]>
2021-04-22 15:13:36 -0300

    Updating the Home files and widgets

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -7,24 +7,27 @@ class QuizCardWidget extends StatelessWidget {
   final String title;
   final String completed;
   final double percent;
+  final VoidCallback onTap;
 
   const QuizCardWidget({
     Key? key, 
     required this.title, 
     required this.completed, 
-    required this.percent
+    required this.percent,
+    required this.onTap    
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(10),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -43,7 +46,7 @@ class QuizCardWidget extends StatelessWidget {
                   child: Text(completed, style: AppTextStyles.body11),
                 ),
                 Expanded(
-                  flex: 4,
+                  flex: 2,
                   child: ProgressIndicatorWidget(value: percent),
                 ),
               ],
