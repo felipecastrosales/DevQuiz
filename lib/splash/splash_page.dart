24bcf43aff21c1bf16c05bb6fd6cb7b347709a8e
Felipe Sales <[soufeliposales@gmail.com]>
2021-04-23 14:57:11 -0300

    Updating the SpashScreen with Simple Animation

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,10 +1,16 @@
-import 'package:devquiz/core/app_gradients.dart';
-import 'package:devquiz/core/app_images.dart';
 import 'package:flutter/material.dart';
 
+import 'package:devquiz/core/app_gradients.dart';
+import 'package:devquiz/core/app_images.dart';
+import 'package:devquiz/home/home_page.dart';
+
 class SplashPage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2)).then((_) => Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => HomePage()),
+    ));
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
